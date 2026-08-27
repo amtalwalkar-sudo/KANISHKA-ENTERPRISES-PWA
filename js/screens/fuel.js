1c12c95d616da9a97b8d7c435baf74d8f15beb49
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-08-27 20:55:19 +0530

    Remove legacy fuel screen logic

--- js/screens/fuel.js
+++
@@ -1,2 +0,0 @@
-import {calculateFuelCost,calculateFuelEfficiency} from '../domain/fuel.js';
-export function createFuelScreen({state}){return{getViewModel(){const s=state.get('fuel')||{};return{...s,cost:calculateFuelCost(s.litres,s.pricePerLitre),efficiency:calculateFuelEfficiency(s.km,s.litres)};}};}
