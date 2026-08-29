313ba10cc860d07c470996cf5cdbc2691518155d
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-08-29 17:13:32 +0530

    chore: remove unused Timeline component

--- src/components/TimelineReadModelStatus.vue
+++
@@ -1,4 +0,0 @@
-<script setup>
-defineProps({ model: { type: Object, default: null } });
-</script>
-<template><span v-if="model" class="sr-only" aria-live="polite">Timeline data: {{ model.events?.length || 0 }} events</span></template>
