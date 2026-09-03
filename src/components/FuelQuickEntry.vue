93b4cc5a5af837e60e3fe3eef4bf67a728e0e850
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-04 03:02:25 +0530

    refactor: remove legacy FuelQuickEntry wrapper

--- src/components/FuelQuickEntry.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-import AuthoritativeRecordForm from './AuthoritativeRecordForm.vue';
-const props=defineProps({authoritativeOdometer:{type:[Number,String],default:null}});
-const emit=defineEmits(['close','saved']);
-</script>
-<template><AuthoritativeRecordForm type="FUEL" mode="CREATE" :authoritative-odometer="props.authoritativeOdometer" @close="emit('close')" @saved="emit('saved',$event)" /></template>
