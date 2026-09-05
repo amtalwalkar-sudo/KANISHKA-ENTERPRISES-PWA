e1ee8de9116c0cc6761876d6776b50008f5c02fd
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-05 13:47:22 +0530

    feat: activate new KFE shell presentation

--- src/presentation/shell/shells/current/CurrentShell.vue
+++ src/presentation/shell/shells/current/CurrentShell.vue
@@ -1,15 +1,7 @@
 <script setup>
-import { onMounted, onUnmounted, ref } from 'vue';
 import App from '../../../../App.vue';
-import WorkBreakControl from '../../../../components/WorkBreakControl.vue';
-
-const showWorkBreak = ref(location.hash === '' || location.hash === '#Work');
-function syncRoute() { showWorkBreak.value = location.hash === '' || location.hash === '#Work'; }
-onMounted(() => window.addEventListener('hashchange', syncRoute));
-onUnmounted(() => window.removeEventListener('hashchange', syncRoute));
 </script>
 
 <template>
   <App />
-  <WorkBreakControl v-if="showWorkBreak" />
 </template>
