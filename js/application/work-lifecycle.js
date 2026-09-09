a69b786ef144165ada8da0dc27ff1244c8af6fff
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-09 12:23:49 +0530

    feat: bind Work lifecycle to atomic outbox

--- js/application/work-lifecycle.js
+++ js/application/work-lifecycle.js
@@ -1,1 +1,8 @@
+import {createWorkApplication as createBaseWorkApplication} from './work-application-lifecycle.js';
+import {createOutboxRepository} from '../core/repository.js';
+
+export function createWorkApplication({repository,telemetry}){
+  return createBaseWorkApplication({repository:createOutboxRepository(repository),telemetry});
+}
+
 export * from './work-application-lifecycle.js';
