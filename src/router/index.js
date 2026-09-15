eb02afd8f94c0752ff34eee7fbf9b0acc284e074
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-15 23:07:04 +0530

    Remove old Performance route for clean rebuild

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WorkModuleView from '../views/WorkModuleView.vue'
-import PerformanceView from '../views/PerformanceView.vue'
 import AdminView from '../views/AdminView.vue'
 
 const routes = [
   { path: '/', name: 'Work', component: WorkModuleView },
-  { path: '/performance', name: 'Performance', component: PerformanceView },
   { path: '/admin', name: 'Admin', component: AdminView }
 ]
 
