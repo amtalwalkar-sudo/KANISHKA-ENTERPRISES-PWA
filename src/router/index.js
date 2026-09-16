c9858fa8aa716e752838431bd0a398dd83e4e8ed
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-16 06:18:08 +0530

    Remove abandoned Stage 6 Ride Capture route

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import WorkModuleView from '../views/WorkModuleView.vue'
 import PerformanceView from '../views/PerformanceView.vue'
 import AdminView from '../views/AdminView.vue'
-import RideCaptureView from '../views/RideCaptureView.vue'
 
 const routes = [
   { path: '/', name: 'Work', component: WorkModuleView },
   { path: '/performance', name: 'Performance', component: PerformanceView },
   { path: '/admin', name: 'Admin', component: AdminView },
-  { path: '/ride-capture', name: 'Ride Capture', component: RideCaptureView },
 ]
 
 const router = createRouter({
