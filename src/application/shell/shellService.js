d274a50a032bfe26c567c78481d8db0c87921cbf
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-16 06:19:35 +0530

    SHELL-006 keep shell startup independent of business stores

--- src/application/shell/shellService.js
+++ src/application/shell/shellService.js
@@ -1,11 +1,8 @@
 import { initializeCanonicalStorage } from '../../utils/indexedDB.js'
-import { useShiftTripStore } from '../../stores/shiftTrip.js'
 
 export const ShellService = Object.freeze({
   async initialize() {
     await initializeCanonicalStorage()
-    const store = useShiftTripStore()
-    await store.initialize()
     return { ready: true }
   },
 })
