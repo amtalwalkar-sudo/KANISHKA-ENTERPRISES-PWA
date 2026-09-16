415c0090927d504599f93aeb4a951b9dc671ebfb
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-16 05:39:44 +0530

    ARCH-009 keep Performance calculation orchestration behind application boundary

--- src/application/performance/performanceService.js
+++ src/application/performance/performanceService.js
@@ -1,7 +1,14 @@
 import { PerformanceRepository } from '../../repositories/performanceRepository.js'
+import { derivePerformance, layerRows, previousRange } from '../../domain/performance/performanceEngine.js'
 
 export const PerformanceService = Object.freeze({
   async getSnapshot() {
     return PerformanceRepository.getSnapshot()
+  },
+  getMetrics(snapshot, range) {
+    return derivePerformance(snapshot, range, previousRange(range))
+  },
+  getLayerRows(card, layer, metrics) {
+    return layerRows(card, layer, metrics)
   },
 })
